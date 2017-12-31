7ce6abccd6ee42e059eb719bf81b86e490e1501c
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-31 16:38:23 +0700

    Notification on web ^^, so simple with fb

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,27 +2,71 @@ import React, { PureComponent, Fragment } from "react"
 import { style as s } from "./style"
 import * as firebase from "firebase"
 import dotenv from "dotenv"
+import fbLogo from "../../img/fb-logo.png"
 
 dotenv.config()
-const { REACT_APP_PROJECT_ID: projectId, REACT_APP_CLIENT_KEY: apiKey } = process.env
+const {
+  REACT_APP_AUTH_DOMAIN: authDomain,
+  REACT_APP_CLIENT_KEY: apiKey,
+  REACT_APP_DATABASE_URL: databaseURL
+} = process.env
 const _ = console.log
+const FB_MAIN_BRANCH = "tmp"
+const VNEXCOIN_TOPIC = "vnexcoin"
 
 export default class App extends PureComponent {
-  state = {}
+  state = {
+    fcmData: null,
+    canPushNotification: null
+  }
+
+  askNotificationPermission = () => {
+    // Let's check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support system notifications")
+      return
+    }
+
+    if (Notification.permission === "granted") {
+      this.setState({ canPushNotification: true })
+      return
+    }
+
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission(permission => {
+        const granted = permission === "granted"
+        if (!granted) return
+        this.setState({ canPushNotification: true })
+      })
+    }
+  }
+
+  notifyMe = payload => {
+    const { canPushNotification } = this.state
+    if (!canPushNotification) return
+    const title = "FCM Push sent"
+    const body = payload.msg || JSON.stringify(payload)
+    const icon = fbLogo
+    new Notification(title, { body, icon })
+  }
 
   componentDidMount() {
-    const authDomain = `${projectId}.firebaseapp.com`
-    const databaseURL = `https://${projectId}.firebaseio.com`
+    _("[ apiKey, authDomain, databaseURL]", apiKey, authDomain, databaseURL)
     const fbApp = firebase.initializeApp({ apiKey, authDomain, databaseURL })
     this.setState({ fbApp })
+
     const db = fbApp.database()
-    const nodeRcRef = db.ref("nodeRemoteCentral")
-    const waitFetchData = new Promise(resolve => nodeRcRef.once("value", snapshot => resolve(snapshot.val())))
-    waitFetchData.then(({ categories, commands }) => {
-      const categoriesArr = Object.values(categories)
-      const commandsArr = Object.values(commands)
-      this.setState({ currCates: categoriesArr, categories: categoriesArr, commands: commandsArr })
+    const refToVnexcoin = db.ref(`${FB_MAIN_BRANCH}/${VNEXCOIN_TOPIC}`)
+
+    refToVnexcoin.on("value", snapshot => {
+      const fcmDataStr = snapshot.val()
+      _(`[fbApp][vnexcoin] snapshot`, fcmDataStr)
+      const fcmData = JSON.parse(fcmDataStr)
+      this.setState({ fcmData })
+      this.notifyMe(fcmData)
     })
+
+    this.askNotificationPermission()
   }
 
   componentWillUnmount() {
@@ -37,6 +81,6 @@ export default class App extends PureComponent {
   }
 
   render() {
-    return <div style={sendMsgDiv} />
+    return <div style={s.rootDiv} />
   }
 }
