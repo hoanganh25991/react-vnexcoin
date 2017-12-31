43453298075bb1de34f0d1993f57c13a2e77cc93
Anh Le Hoang <lehoanganh25991@gmail.com>
2017-12-31 09:31:10 +0700

    Connect to fb

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,44 +1,42 @@
 import React, { PureComponent, Fragment } from "react"
 import { style as s } from "./style"
-import LoginPage from "../LoginPage"
-import SendMessage from "../SendMessage"
-import { getPageAccessTokens } from "../../facebook-api/pageInsights"
+import * as firebase from "firebase"
+import dotenv from "dotenv"
 
+dotenv.config()
+const { REACT_APP_PROJECT_ID: projectId, REACT_APP_CLIENT_KEY: apiKey } = process.env
 const _ = console.log
 
 export default class App extends PureComponent {
-  state = {
-    pageAccessTokens: null
+  state = {}
+
+  componentDidMount() {
+    const authDomain = `${projectId}.firebaseapp.com`
+    const databaseURL = `https://${projectId}.firebaseio.com`
+    const fbApp = firebase.initializeApp({ apiKey, authDomain, databaseURL })
+    this.setState({ fbApp })
+    const db = fbApp.database()
+    const nodeRcRef = db.ref("nodeRemoteCentral")
+    const waitFetchData = new Promise(resolve => nodeRcRef.once("value", snapshot => resolve(snapshot.val())))
+    waitFetchData.then(({ categories, commands }) => {
+      const categoriesArr = Object.values(categories)
+      const commandsArr = Object.values(commands)
+      this.setState({ currCates: categoriesArr, categories: categoriesArr, commands: commandsArr })
+    })
   }
 
-  componentDidMount() {}
-
-  cbUserAccessToken = async token => {
-    const { pageAccessTokens } = await getPageAccessTokens(token)
-    _("[pageAccessTokens]", pageAccessTokens)
-    this.setState({ pageAccessTokens })
+  componentWillUnmount() {
+    const { fbApp } = this.state
+    if (!fbApp) return
+    fbApp
+      .delete()
+      .then(() => _("[fbApp] deleted"))
+      .catch(err => {
+        _("[fbApp] Fail to delete app", err)
+      })
   }
 
   render() {
-    const { pageAccessTokens } = this.state
-    const loginPage = !pageAccessTokens
-    const hasPages = pageAccessTokens && pageAccessTokens.length > 0
-    const sendMsgDiv = hasPages ? { ...s.sendMsgDiv, ...s.sendMsgDivHasData } : s.sendMsgDiv
-
-    return (
-      <div style={sendMsgDiv}>
-        {loginPage && <LoginPage cbToken={this.cbUserAccessToken} />}
-        {!loginPage && !hasPages && "Dont have page to send message."}
-        {hasPages && <div style={s.title}>Your Pages</div>}
-        {hasPages && (
-          <Fragment>
-            {pageAccessTokens.map(page => {
-              const { id: pageId } = page
-              return <SendMessage key={pageId} page={page} />
-            })}
-          </Fragment>
-        )}
-      </div>
-    )
+    return <div style={sendMsgDiv} />
   }
 }
